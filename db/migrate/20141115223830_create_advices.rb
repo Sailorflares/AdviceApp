71d203e4b07db7a2b0a948f4622bae1a716e80df
Elizabeth Wolensky <liz.wolensky@gmail.com>
2014-11-15 17:46:58 -0500

    first commit

---
+++ db/migrate/20141115223830_create_advices.rb
@@ -0,0 +1,11 @@
+class CreateAdvices < ActiveRecord::Migration
+  def change
+    create_table :advices do |t|
+      t.text :advice_text
+      t.text :url
+      
+
+      t.timestamps
+    end
+  end
+end
