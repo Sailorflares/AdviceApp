2a8815df3f194a9da118962c2866bab78006a0d3
Elizabeth Wolensky <liz.wolensky@gmail.com>
2014-11-15 21:28:52 -0500

    recommit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], {:display => "popup", :provider_ignores_state => true}
+  provider :facebook, ENV['FB_APP_ID'], ENV['FB_APP_SECRET'],
+  :display => 'popup'
 end
