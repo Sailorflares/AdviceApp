f52739fcff6fe565c94eb3c56bedd1774e81f419
sofia ma <sofia.ma@gmail.com>
2014-11-19 20:37:13 -0500

    deleted vote model and controller

--- app/models/advice.rb
+++ app/models/advice.rb
@@ -19,4 +19,8 @@ class Advice < ActiveRecord::Base
       errors.add(:advice_text, "Sorry, that advice doesn't seem to be in the URL you provided.")
     end
   end
+
+  def update_votes
+    
+  end
 end
