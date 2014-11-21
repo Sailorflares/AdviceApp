29f5fe95d1cdcf1914dd6f86b8d47861b7530511
sofia ma <sofia.ma@gmail.com>
2014-11-20 20:00:55 -0500

    fixed it this this, previous commit was incorrect

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -14,7 +14,7 @@ class AdvicesController < ApplicationController
     @advice = Advice.find_or_create_by(advice_params)
     if @advice# this path means the advice was found or newly created
       #save initial user_advice data
-      current_user.user_advices.create(advice_id: @advice.id)
+      current_user.user_advices.find_or_create_by(advice_id: @advice.id)
       redirect_to user_path(current_user)
     else #this means something went wrong
       redirect_to new_advice_path
@@ -23,13 +23,11 @@ class AdvicesController < ApplicationController
 
   def vote
     @advice = Advice.find(params[:id])
-    @advice.
   end
 
   private
-
-  def advice_params
-    params.require(:advice).permit(:url)
-  end
+    def advice_params
+      params.require(:advice).permit(:url)
+    end
 
 end
