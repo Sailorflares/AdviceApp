e973df82098a33ab0368019aeb6438e63ef61f90
sofia ma <sofia.ma@gmail.com>
2014-11-20 21:39:34 -0500

    removed resources upvotes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
-  resources :upvotes
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
