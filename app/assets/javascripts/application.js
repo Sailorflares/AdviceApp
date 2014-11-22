080c8dd4d759b61382aa4119efafdeccf010b8e8
sofia ma <sofia.ma@gmail.com>
2014-11-22 17:17:32 -0500

    fixed homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+//= require_self
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1491949827760019',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.0' // use version 2.0
+  });
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
